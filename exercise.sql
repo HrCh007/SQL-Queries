33bebc9614444c028682ec9843b3e12bf2eb1bff
HrCh007 <chowdhryhrithik@gmail.com>
2022-07-16 20:54:29 +0530

    third commit

--- exercise.sql
+++ exercise.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE IF NOT EXISTS office;
+
 USE office;
+
 CREATE TABLE Employee(
 	ID INT NOT NULL,
     Name VARCHAR (255),
@@ -122,8 +124,17 @@ GROUP BY E.Location
 ORDER BY COUNT(S.ID) ASC
 LIMIT 1;
 
--- 
+-- Print name of employees who have not completed hours (8:30 hoursrs) 
 
-SELECT S1.*, S2.*, (S2.Swipe_time-S1.Swipe_time) as Total_hours
-FROM Swipes S1, Swipes S2
-WHERE S1.ID = S2.ID AND S1.Swipe_Date = S2.Swipe_Date AND S1.Swipe_time != S2.Swipe_time AND S2.swipe_time > S1.Swipe_time;
+SELECT E.name, S2.Swipe_Date, TIMEDIFF(S2.swipe_time,S1.swipe_time) as Total_hours
+FROM Swipes S1
+JOIN Swipes S2
+	ON S1.ID = S2.ID 
+    AND S1.Swipe_Date = S2.Swipe_Date 
+    AND S1.Swipe_time != S2.Swipe_time 
+    AND S2.swipe_time > S1.Swipe_time 
+    AND TIMEDIFF(S2.swipe_time,S1.swipe_time) <= '08:30:00'
+JOIN Employee E 
+	ON S1.ID = E.ID 
+    AND S2.ID = E.ID;
+
