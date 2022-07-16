298a6ed523685b84768ea976d8d3875b5e8b0fd4
HrCh007 <chowdhryhrithik@gmail.com>
2022-07-16 21:09:46 +0530

    fourth commit

--- exercise.sql
+++ exercise.sql
@@ -124,9 +124,23 @@ GROUP BY E.Location
 ORDER BY COUNT(S.ID) ASC
 LIMIT 1;
 
--- Print name of employees who have not completed hours (8:30 hoursrs) 
+-- Print name of employees who have not completed hours (8:30 hours) 
 
 SELECT E.name, S2.Swipe_Date, TIMEDIFF(S2.swipe_time,S1.swipe_time) as Total_hours
+FROM Swipes S1
+JOIN Swipes S2
+	ON S1.ID = S2.ID 
+    AND S1.Swipe_Date = S2.Swipe_Date 
+    AND S1.Swipe_time != S2.Swipe_time 
+    AND S2.swipe_time > S1.Swipe_time 
+    AND TIMEDIFF(S2.swipe_time,S1.swipe_time) <= '08:30:00'
+JOIN Employee E 
+	ON S1.ID = E.ID 
+    AND S2.ID = E.ID;
+    
+-- Total number of employees who have not completed hours
+
+SELECT COUNT(DISTINCT E.ID) AS No_of_employees_not_completing_hours           
 FROM Swipes S1
 JOIN Swipes S2
 	ON S1.ID = S2.ID 
