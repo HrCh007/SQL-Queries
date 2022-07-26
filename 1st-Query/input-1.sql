dcb9a634ef7e00ff357d56896d4193c35747ce0c
HrCh007 <chowdhryhrithik@gmail.com>
2022-07-26 13:20:18 +0530

    modified 1st Query

--- 1st-Query/input-1.sql
+++ 1st-Query/input-1.sql
@@ -1,4 +1,4 @@
--- Print name of employees that worked at least 2 days from office.
+-- Print name of employees that worked at least 2 days from office.(Assuming an employee swipes exactly two times a day)
 
 SELECT E.Name
 FROM Employee E
@@ -6,3 +6,12 @@ JOIN Swipes S
 	ON E.ID = S.ID    
 GROUP BY S.ID
 HAVING COUNT(S.ID) > 3;
+
+-- Print name of employees that worked at least 2 days from office.(Without the above assumption)
+
+SELECT E.Name
+FROM Employee E
+JOIN Swipes S 
+	ON E.ID = S.ID
+GROUP BY E.Name
+HAVING COUNT(DISTINCT S.Swipe_Date) > 1;
