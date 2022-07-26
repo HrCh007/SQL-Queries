0a68194382c359bf7ae093b563ace53e37e8850b
HrCh007 <chowdhryhrithik@gmail.com>
2022-07-26 21:22:32 +0530

    modified fourth query

--- 4th-Query/input-4.sql
+++
@@ -1,9 +0,0 @@
--- Print Location where most employees are working from home
-
-SELECT E.Location
-FROM Employee E 
-JOIN Swipes S 
-	ON E.ID = S.ID
-GROUP BY E.Location
-ORDER BY COUNT(S.ID) ASC
-LIMIT 1;
--- _all.sql
+++ _all.sql
@@ -110,7 +110,7 @@ JOIN Swipes S
 GROUP BY E.Name
 HAVING COUNT(DISTINCT S.Swipe_Date) > 1;
 
--- Print Name and number of days present in office for a employee
+-- Print Name and number of days present in office for a employee.(Assuming an employee swipes exactly two times a day)
 
 SELECT E.Name, COUNT(S.ID)/2 AS "Days Present in Office"
 FROM Employee E
@@ -118,9 +118,17 @@ LEFT JOIN Swipes S
 	ON E.ID = S.ID
 GROUP BY E.ID;
 
+-- Print Name and number of days present in office for a employee.(Without the above assumption)
+
+SELECT E.Name, COUNT(DISTINCT S.Swipe_Date) AS "Days Present in Office"
+FROM Employee E
+LEFT JOIN Swipes S 
+	ON E.ID = S.ID
+GROUP BY E.Name;
+
 -- Print Name of employees working from home
 
-SELECT E.name
+SELECT E.name,E.LOCATION
 FROM Employee E
 LEFT JOIN Swipes S 
 	ON E.ID = S.ID
@@ -133,14 +141,14 @@ LEFT JOIN Swipes S
 	ON E.ID = S.ID
 WHERE S.ID IS NULL;
 
--- Print Location where most employees ae working from home
+-- Print Location where most employees are working from home
 
-SELECT E.Location
+SELECT E.Location -- , COUNT(DISTINCT E.Name)*3 - COUNT(DISTINCT S.ID, S.Swipe_Date) AS Number_of_WFH_SHIFTS 
 FROM Employee E 
-JOIN Swipes S 
+LEFT JOIN Swipes S 
 	ON E.ID = S.ID
 GROUP BY E.Location
-ORDER BY COUNT(S.ID) ASC
+ORDER BY COUNT(DISTINCT E.Name)*3 - COUNT(DISTINCT S.ID, S.Swipe_Date) DESC
 LIMIT 1;
 
 -- Print name of employees who have not completed minimum hours (8:30 hours) 
@@ -150,7 +158,6 @@ FROM Swipes S1
 JOIN Swipes S2
 	ON S1.ID = S2.ID 
     AND S1.Swipe_Date = S2.Swipe_Date 
-    AND S1.Swipe_time != S2.Swipe_time 
     AND S2.swipe_time > S1.Swipe_time 
     AND TIMEDIFF(S2.swipe_time,S1.swipe_time) <= '08:30:00'
 JOIN Employee E 
@@ -164,9 +171,7 @@ FROM Swipes S1
 JOIN Swipes S2
 	ON S1.ID = S2.ID 
     AND S1.Swipe_Date = S2.Swipe_Date 
-    AND S1.Swipe_time != S2.Swipe_time 
     AND S2.swipe_time > S1.Swipe_time 
     AND TIMEDIFF(S2.swipe_time,S1.swipe_time) <= '08:30:00';
-
 
 
